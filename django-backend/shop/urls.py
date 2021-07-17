ed4e391a6c158fb358d2f58d1faf264a19ee753d
PASBdAN <petroniloneto.am@hotmail.com>
2021-07-16 21:28:25 -0400

    including method to add new routes for media files from MEDIA_ROOT path

--- django-backend/shop/urls.py
+++ django-backend/shop/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from rest_framework import routers
 from products.api import viewsets as productsviewsets
 route = routers.DefaultRouter()
@@ -25,4 +28,4 @@ route.register(r'products',productsviewsets.ProductsViewSet, basename="Products"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
