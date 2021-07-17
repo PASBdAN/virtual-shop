947c313457e35ac1ba1ef44a820ddf619e9cd785
PASBdAN <petroniloneto.am@hotmail.com>
2021-07-17 18:32:33 -0400

    modifying products model fields to inherit TrackingModel timestamp fields

--- django-backend/products/models.py
+++ django-backend/products/models.py
@@ -1,20 +1,19 @@
 from django.db import models
+from helpers.models import TrackingModel
 from uuid import uuid4
-from datetime import date, datetime
 
 # Create your models here.
 
-def upload_image_product(instance,filename):
+def upload_image_product(instance, filename):
     '''
     Função necessária para evitar ter nomes de arquivos duplicados:
     Cocatena-se o id do produto/objeto no nome do arquivo.
     '''
     return f'{instance.id_product}-{filename}'
 
-class Products(models.Model):
+class Products(TrackingModel):
     id_product = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255, blank=False)
     description = models.CharField(max_length=1020, blank=False)
     price = models.FloatField(blank=False)
     image = models.ImageField(upload_to=upload_image_product)
-    created_at = models.DateTimeField(auto_now_add=True)
